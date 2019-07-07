dad1afd00e2ef5240af9cd64ad4951f8c4dd2f64
Masahiro Honma <hiratara@cpan.org>
2019-07-07 21:30:50 +0900

    feat: add an answer for http://nabetani.sakura.ne.jp/hena/ord12rotdice/

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,60 @@
+pub struct Dice {
+    up: isize,
+    left: isize,
+    right: isize,
+}
+
+impl Dice {
+    pub fn new() -> Self {
+        Dice {
+            up: 1,
+            left: 3,
+            right: 5,
+        }
+    }
+
+    pub fn north(&mut self) {
+        let orig_up = self.up;
+        self.up = self.right;
+        self.right = 7 - orig_up;
+    }
+
+    pub fn east(&mut self) {
+        let orig_up = self.up;
+        self.up = self.left;
+        self.left = 7 - orig_up;
+    }
+
+    pub fn south(&mut self) {
+        self.north();
+        self.north();
+        self.north();
+    }
+
+    pub fn west(&mut self) {
+        self.east();
+        self.east();
+        self.east();
+    }
+}
+
 pub fn solve(input: &str) -> String {
-    input.to_string()
+    let mut result = String::new();
+
+    let mut dice = Dice::new();
+    result.push_str(&format!("{}", dice.up));
+    for c in input.chars() {
+        match c {
+            'N' => dice.north(),
+            'E' => dice.east(),
+            'S' => dice.south(),
+            'W' => dice.west(),
+            _ => unimplemented!(),
+        };
+        result.push_str(&format!("{}", dice.up));
+    }
+
+    result
 }
 
 #[cfg(test)]
