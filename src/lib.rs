074fd1be0a560b6f76933e3ec06201734c24b16e
Masahiro Honma <hiratara@cpan.org>
2019-07-07 21:08:13 +0900

    feat: implement templates for offline DOUKAKU

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+pub fn solve(input: &str) -> String {
+    input.to_string()
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::{Result, BufReader, BufRead};
+    use std::fs::File;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_solve() -> Result<()> {
+        let file = File::open("test.tsv")?;
+        let buf_reader = BufReader::new(file);
+        for line in buf_reader.lines() {
+            let line = line?;
+            let cols = line.split('\t').collect::<Vec<&str>>();
+            assert_eq!(crate::solve(cols[1]), cols[2]);
+        }
+
+        Ok(())
     }
 }
